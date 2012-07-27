73e3989bd6e2e0793f6d98a07301b9016fdf9fd8
robert Reed <callmeelasinthe@gmail.com>
2012-07-26 19:48:55 -0500

    Download files from class webpage

---
+++ Graph.h
@@ -0,0 +1,224 @@
+// ----------------------
+// projects/graph/Graph.h
+// Copyright (C) 2012
+// Glenn P. Downing
+// ----------------------
+
+#ifndef Graph_h
+#define Graph_h
+
+// --------
+// includes
+// --------
+
+#include <cassert> // assert
+#include <cstddef> // size_t
+#include <utility> // make_pair, pair
+#include <vector>  // vector
+
+// -----
+// Graph
+// -----
+
+class Graph {
+    public:
+        // --------
+        // typedefs
+        // --------
+
+        typedef int vertex_descriptor;  // fix!
+        typedef int edge_descriptor;    // fix!
+
+        typedef int* vertex_iterator;    // fix!
+        typedef int* edge_iterator;      // fix!
+        typedef int* adjacency_iterator; // fix!
+
+        typedef std::size_t vertices_size_type;
+        typedef std::size_t edges_size_type;
+
+    public:
+        // --------
+        // add_edge
+        // --------
+
+        /**
+         * <your documentation>
+         */
+        friend std::pair<edge_descriptor, bool> add_edge (vertex_descriptor, vertex_descriptor, Graph&) {
+            // <your code>
+            edge_descriptor ed;
+            bool            b;
+            return std::make_pair(ed, b);}
+
+        // ----------
+        // add_vertex
+        // ----------
+
+        /**
+         * <your documentation>
+         */
+        friend vertex_descriptor add_vertex (Graph&) {
+            // <your code>
+            vertex_descriptor v;
+            return v;}
+
+        // -----------------
+        // adjacent_vertices
+        // -----------------
+
+        /**
+         * <your documentation>
+         */
+        friend std::pair<adjacency_iterator, adjacency_iterator> adjacent_vertices (vertex_descriptor, const Graph&) {
+            // <your code>
+            adjacency_iterator b = adjacency_iterator();
+            adjacency_iterator e = adjacency_iterator();
+            return std::make_pair(b, e);}
+
+        // ----
+        // edge
+        // ----
+
+        /**
+         * <your documentation>
+         */
+        friend std::pair<edge_descriptor, bool> edge (vertex_descriptor, vertex_descriptor, const Graph&) {
+            // <your code>
+            edge_descriptor ed;
+            bool            b;
+            return std::make_pair(ed, b);}
+
+        // -----
+        // edges
+        // -----
+
+        /**
+         * <your documentation>
+         */
+        friend std::pair<edge_iterator, edge_iterator> edges (const Graph&) {
+            // <your code>
+            edge_iterator b;
+            edge_iterator e;
+            return std::make_pair(b, e);}
+
+        // ---------
+        // num_edges
+        // ---------
+
+        /**
+         * <your documentation>
+         */
+        friend edges_size_type num_edges (const Graph&) {
+            // <your code>
+            edges_size_type s;
+            return s;}
+
+        // ------------
+        // num_vertices
+        // ------------
+
+        /**
+         * <your documentation>
+         */
+        friend vertices_size_type num_vertices (const Graph&) {
+            // <your code>
+            vertices_size_type s;
+            return s;}
+
+        // ------
+        // source
+        // ------
+
+        /**
+         * <your documentation>
+         */
+        friend vertex_descriptor source (edge_descriptor, const Graph&) {
+            // <your code>
+            vertex_descriptor v;
+            return v;}
+
+        // ------
+        // target
+        // ------
+
+        /**
+         * <your documentation>
+         */
+        friend vertex_descriptor target (edge_descriptor, const Graph&) {
+            // <your code>
+            vertex_descriptor v;
+            return v;}
+
+        // ------
+        // vertex
+        // ------
+
+        /**
+         * <your documentation>
+         */
+        friend vertex_descriptor vertex (vertices_size_type, const Graph&) {
+            // <your code>
+            vertex_descriptor vd;
+            return vd;}
+
+        // --------
+        // vertices
+        // --------
+
+        /**
+         * <your documentation>
+         */
+        friend std::pair<vertex_iterator, vertex_iterator> vertices (const Graph&) {
+            // <your code>
+            vertex_iterator b = vertex_iterator();
+            vertex_iterator e = vertex_iterator();
+            return std::make_pair(b, e);}
+
+    private:
+        // ----
+        // data
+        // ----
+
+        std::vector< std::vector<vertex_descriptor> > g; // something like this
+
+        // -----
+        // valid
+        // -----
+
+        /**
+         * <your documentation>
+         */
+        bool valid () const {
+            // <your code>
+            return true;}
+
+    public:
+        // ------------
+        // constructors
+        // ------------
+
+        /**
+         * <your documentation>
+         */
+        Graph () {
+            // <your code>
+            assert(valid());}
+
+        // Default copy, destructor, and copy assignment
+        // Graph  (const Graph<T>&);
+        // ~Graph ();
+        // Graph& operator = (const Graph&);
+    };
+
+// ---------
+// has_cycle
+// ---------
+
+/**
+ * depth-first traversal
+ * three colors
+ * <your documentation>
+ */
+template <typename G>
+bool has_cycle (const G& g) {
+    return true;}
